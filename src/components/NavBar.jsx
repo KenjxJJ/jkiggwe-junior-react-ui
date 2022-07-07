dc32f8628f8238622db621db64c60b6ce273670f
KenjxJJ <joelkiggwe94@gmail.com>
2022-07-07 23:36:14 +0300

    First look of the navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,23 +10,24 @@ export class NavBarComponent extends Component {
         <header>
           <div className="category-name">
             <span>
-              <a href="#">Women</a>
+             Women
             </span>
             <span>
-              <a href="#">Men</a>
+              Men
             </span>
             <span>
-              <a href="#">Kids</a>
+              Kids
             </span>
           </div>
-          <div className="logo">
-            <img src={logoIcon} alt="Logo"/>
+          <div id="logo-wrapper">
+            <img id="logo" src={logoIcon} alt="Logo"/>
           </div>
           <div className="category-action-buttons">
             <span name="currency-switcher" 
               id="currency-switcher">$
+              {/* <span></span> */}
             </span>
-            <span><img src={cartIcon} alt="cart-icon"/></span>
+            <span id="cart-btn"><img src={cartIcon} alt="cart-icon"/></span>
           </div>
         </header>
       </>
